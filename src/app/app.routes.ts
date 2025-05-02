4c7ebec39dd61d76db0a8e36c727fbd9a3ee3d46
HumbertoRC <luis.rivera0834@alumnos.udg.mx>
2025-05-02 01:03:34 -0600

    Correccion de ruta de pagina de registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { LoginComponent } from './pages/login/login.component';
 import { IndexComponent } from './pages/index/index.component';
 import { SuscripcionComponent } from './pages/suscripcion/suscripcion.component';
 import { AyudaComponent } from './pages/ayuda/ayuda.component';
-import { RegisterComponent } from './register/register.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
